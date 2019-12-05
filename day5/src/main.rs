80669da6ae0b535294a26d986bff907e00d1bed1
Justin Horton <18197817+justinhorton@users.noreply.github.com>
2019-12-05 14:37:23 -0800

    Day 5 part 1

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -1,12 +1,14 @@
 use std::io::stdin;
 use std::collections::VecDeque;
 use std::ops::{Add, AddAssign};
+use std::error::Error;
+use std::fmt::{Display, Formatter};
 
-const INPUT: &str = "3,0,4,0,99";
+const INPUT: &str = include_str!("../input.txt");
 
 fn main() {
-    println!("Day 5-1: {}", calc_day5());
-    println!("DSI {:?}", destructure_inst(1002))
+    run_diagnostic();
+//    println!("DSI {:?}", destructure_inst(1002))
 }
 
 // OPCODE 1: 1,p1,p2,dest; mem[dest] = mem[p1] + mem[p2]
@@ -15,50 +17,88 @@ fn main() {
 // OPCODE 4: 4,p1; <output mem[p1]>
 // OPCODE 99: halt
 // OPCODE anything else: error
-fn calc_day5() -> i32 {
+
+// want instrs:
+//   ADD((p1, mode), (p2, mode), (dst, mode = POSITION))
+
+fn get_parameter(pc: usize, memory: &Vec<i32>, p_index: usize, inst: &Instruction) -> i32 {
+    let mode = inst.addr_modes.get(p_index);
+    match mode {
+        Some(AddressingMode::IMMEDIATE) => memory[pc + 1 + p_index],
+        Some(AddressingMode::POSITION) => memory[memory[pc + 1 + p_index] as usize],
+        _ => panic!("panic!!!")
+    }
+}
+
+fn run_diagnostic() -> Result<i32, String> {
     let input: Vec<&str> = INPUT.trim().split(',').collect();
     let mut memory: Vec<i32> = input
         .iter()
         .map(|it| it.parse::<i32>().expect("Can't parse int"))
         .collect();
 
-    let mut pos = 0;
-    while pos < memory.len() {
-        let opcode = memory[pos];
-        match opcode {
-            1..=2 => {
-                let op1_pos = memory[pos + 1] as usize;
-                let op2_pos = memory[pos + 2] as usize;
-                let dest_pos = memory[pos + 3] as usize;
-                let op1 = memory[op1_pos];
-                let op2 = memory[op2_pos];
+    let mut pc = 0;
+    while pc < memory.len() {
+        let instruction = destructure_inst(memory[pc]);
+        match instruction {
+            Ok(inst) => {
+                match inst.opcode {
+                    OP_ADD => {
+                        let r0 = get_parameter(pc, &memory, 0, &inst);
+                        let r1 = get_parameter(pc, &memory, 1, &inst);
+                        let dest_pos = memory[pc + 3] as usize;
 
-                let result = if opcode == 1 { op1 + op2 } else { op1 * op2 };
-                memory[dest_pos] = result;
+                        memory[dest_pos] = r0 + r1;
+                        pc += 4;
+                    }
+                    OP_MUL => {
+                        let r0 = get_parameter(pc, &memory, 0, &inst);
+                        let r1 = get_parameter(pc, &memory, 1, &inst);
+                        let dest_pos = memory[pc + 3] as usize;
 
-                pos += 4;
+                        memory[dest_pos] = r0 * r1;
+                        pc += 4;
+                    }
+                    OP_INPUT => {
+                        let mut s = String::new();
+                        println!("Input value: ");
+                        stdin().read_line(&mut s).expect("Failed to read line");
+                        let value = s.trim().parse::<i32>().expect("Bad input integer");
+
+                        let dest_pos = memory[pc + 1] as usize;
+                        memory[dest_pos] = value;
+                        pc += 2;
+                    }
+                    OP_OUTPUT => {
+                        let output = match inst.addr_modes.get(0).unwrap() {
+                            AddressingMode::IMMEDIATE => memory[pc + 1],
+                            AddressingMode::POSITION => memory[memory[pc + 1] as usize]
+                        };
+                        println!("{}", output);
+                        pc += 2;
+                    }
+                    OP_HALT => return Ok(memory[0]),
+                    x => {
+                        return Err(format!("Unknown opcode: {}", x))
+                    }
+                };
+//                println!("opcode: {}", inst.opcode);
             }
-            3 => {
-                let mut s = String::new();
-                println!("Input value: ");
-                stdin().read_line(&mut s);
-                let value = s.trim().parse::<i32>().expect("Bad input value");
-
-                let dest_pos = memory[pos + 1] as usize;
-                memory[dest_pos] = value;
-                pos += 2;
-            }
-            4 => {
-                let output_pos = memory[pos + 1] as usize;
-
-                println!("{}", memory[output_pos]);
-                pos += 2;
-            }
-            99 => return memory[0],
-            x => panic!("Unknown opcode {}!", x),
-        };
+            Err(e) => panic!(e)
+        }
     }
-    memory[0]
+    Ok(memory[0])
+}
+
+#[derive(Debug)]
+struct Instruction {
+    opcode: i8,
+    addr_modes: Vec<AddressingMode>
+}
+
+struct Parameter {
+    mode: AddressingMode,
+    value: i32 // interpret based on mode
 }
 
 #[derive(Debug)]
@@ -77,17 +117,44 @@ impl AddressingMode {
     }
 }
 
-fn destructure_inst(inst: i32) -> (i8, AddressingMode, AddressingMode, AddressingMode) {
+#[derive(Debug)]
+struct InvalidOpcode {
+
+}
+
+impl Error for InvalidOpcode {
+    fn description(&self) -> &str {
+        "Invalid Opcode"
+    }
+}
+
+impl Display for InvalidOpcode {
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result<(), std::fmt::Error> {
+        write!(f, "Invalid opcode")
+    }
+}
+
+const OP_ADD: i8 = 1;
+const OP_MUL: i8 = 2;
+const OP_INPUT: i8 = 3;
+const OP_OUTPUT: i8 = 4;
+const OP_HALT: i8 = 99;
+
+fn destructure_inst(inst: i32) -> Result<Instruction, InvalidOpcode> {
     let mut digits = digits(inst);
-    let opcode_0 = digits.pop_back().unwrap();
-    let opcode_1 = digits.pop_back().unwrap();
+//    println!("digits: {:?}", digits);
+
+    let mut addr_modes: Vec<AddressingMode> = Vec::new();
+
+    let opcode_0 = digits.pop_back().unwrap_or_default();
+    let opcode_1 = digits.pop_back().unwrap_or_default();
     let opcode: i8 = opcode_1 * 10 + opcode_0;
 
-    let p1_addressing = AddressingMode::from(digits.pop_back());
-    let p2_addressing = AddressingMode::from(digits.pop_back());
-    let p3_addressing = AddressingMode::from(digits.pop_back());
+    for _i in 0..3 {
+        addr_modes.push(AddressingMode::from(digits.pop_back()));
+    }
 
-    return (opcode, p1_addressing, p2_addressing, p3_addressing)
+    Ok(Instruction { opcode, addr_modes })
 }
 
 fn digits(num: i32) -> VecDeque<i8> {
