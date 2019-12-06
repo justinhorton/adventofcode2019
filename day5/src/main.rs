db97ee665bee327ff0d03af73a5a60c987ee8cae
Justin Horton <18197817+justinhorton@users.noreply.github.com>
2019-12-05 21:43:13 -0800

    Day 5: restructure opcode and parm handling

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -1,36 +1,19 @@
-use std::io::stdin;
 use std::collections::VecDeque;
-use std::ops::{Add, AddAssign};
-use std::error::Error;
+use std::convert::TryInto;
 use std::fmt::{Display, Formatter};
+use std::fs::ReadDir;
+use std::io::stdin;
 
-const INPUT: &str = include_str!("../input.txt");
+const INPUT: &str = include_str!("../day5.txt");
 
 fn main() {
-    run_diagnostic();
-//    println!("DSI {:?}", destructure_inst(1002))
-}
-
-// OPCODE 1: 1,p1,p2,dest; mem[dest] = mem[p1] + mem[p2]
-// OPCODE 2: 2,p1,p2,dest; mem[dest] = mem[p1] * mem[p2]
-// OPCODE 3: 3,p1; mem[p1] = <input i32>
-// OPCODE 4: 4,p1; <output mem[p1]>
-// OPCODE 99: halt
-// OPCODE anything else: error
-
-// want instrs:
-//   ADD((p1, mode), (p2, mode), (dst, mode = POSITION))
-
-fn get_parameter(pc: usize, memory: &Vec<i32>, p_index: usize, inst: &Instruction) -> i32 {
-    let mode = inst.addr_modes.get(p_index);
-    match mode {
-        Some(AddressingMode::IMMEDIATE) => memory[pc + 1 + p_index],
-        Some(AddressingMode::POSITION) => memory[memory[pc + 1 + p_index] as usize],
-        _ => panic!("panic!!!")
+    match run_diagnostic() {
+        Ok(()) => println!("Diagnostic completed"),
+        Err(e) => println!("Diagnostic failed\n{}", e),
     }
 }
 
-fn run_diagnostic() -> Result<i32, String> {
+fn run_diagnostic() -> Result<(), String> {
     let input: Vec<&str> = INPUT.trim().split(',').collect();
     let mut memory: Vec<i32> = input
         .iter()
@@ -42,159 +25,207 @@ fn run_diagnostic() -> Result<i32, String> {
         let instruction = destructure_inst(memory[pc]);
         match instruction {
             Ok(inst) => {
-                match inst.opcode {
-                    OP_ADD => {
-                        let r0 = get_parameter(pc, &memory, 0, &inst);
-                        let r1 = get_parameter(pc, &memory, 1, &inst);
-                        let dest_pos = memory[pc + 3] as usize;
+                println!("{:?}", inst);
 
-                        memory[dest_pos] = r0 + r1;
+                let r0 = get_parameter(pc, &memory, 0, &inst);
+                let r1 = get_parameter(pc, &memory, 1, &inst);
+                let r2 = get_parameter(pc, &memory, 2, &inst);
+                match inst.op {
+                    Op::Add => {
+                        memory[r2.unwrap() as usize] = r0.unwrap() + r1.unwrap();
                         pc += 4;
                     }
-                    OP_MUL => {
-                        let r0 = get_parameter(pc, &memory, 0, &inst);
-                        let r1 = get_parameter(pc, &memory, 1, &inst);
-                        let dest_pos = memory[pc + 3] as usize;
-
-                        memory[dest_pos] = r0 * r1;
+                    Op::Mul => {
+                        memory[r2.unwrap() as usize] = r0.unwrap() * r1.unwrap();
                         pc += 4;
                     }
-                    OP_INPUT => {
+                    Op::Input => {
+                        println!("Input an integer: ");
+
                         let mut s = String::new();
-                        println!("Input value: ");
-                        stdin().read_line(&mut s).expect("Failed to read line");
-                        let value = s.trim().parse::<i32>().expect("Bad input integer");
+                        let read_line = stdin().read_line(&mut s);
+                        if read_line.is_err() {
+                            return Err("Error parsing input line".to_string());
+                        }
 
-                        let dest_pos = memory[pc + 1] as usize;
-                        memory[dest_pos] = value;
-                        pc += 2;
-                    }
-                    OP_OUTPUT => {
-                        let output = match inst.addr_modes.get(0).unwrap() {
-                            AddressingMode::IMMEDIATE => memory[pc + 1],
-                            AddressingMode::POSITION => memory[memory[pc + 1] as usize]
+                        let int_val = match s.trim().parse::<i32>() {
+                            Ok(x) => x,
+                            Err(e) => return Err(e.to_string()),
                         };
-                        println!("{}", output);
+
+                        let dst = memory[pc + 1] as usize;
+                        memory[dst] = int_val;
                         pc += 2;
                     }
-                    OP_JIT => {
-                        let r0 = get_parameter(pc, &memory, 0, &inst);
-                        let new_pc = get_parameter(pc, &memory, 1, &inst) as usize;
-
-                        pc = if r0 != 0 { new_pc } else { pc + 3 };
+                    Op::Output => {
+                        println!("{}", r0.unwrap());
+                        pc += 2;
                     }
-                    OP_JIF => {
-                        let r0 = get_parameter(pc, &memory, 0, &inst);
-                        let new_pc = get_parameter(pc, &memory, 1, &inst) as usize;
-
-                        pc = if r0 == 0 { new_pc } else { pc + 3 };
+                    Op::Jit => {
+                        let new_pc = r1.unwrap() as usize;
+                        pc = if r0.unwrap() != 0 { new_pc } else { pc + 3 };
                     }
-                    OP_JLT => {
-                        let r0 = get_parameter(pc, &memory, 0, &inst);
-                        let r1 = get_parameter(pc, &memory, 1, &inst);
-                        let dest_pos = memory[pc + 3] as usize;
-
-                        memory[dest_pos] = if r0 < r1 { 1 } else { 0 };
+                    Op::Jif => {
+                        let new_pc = r1.unwrap() as usize;
+                        pc = if r0.unwrap() == 0 { new_pc } else { pc + 3 };
+                    }
+                    Op::Lt => {
+                        memory[r2.unwrap() as usize] =
+                            if r0.unwrap() < r1.unwrap() { 1 } else { 0 };
                         pc += 4;
                     }
-                    OP_EQ => {
-                        let r0 = get_parameter(pc, &memory, 0, &inst);
-                        let r1 = get_parameter(pc, &memory, 1, &inst);
-                        let dest_pos = memory[pc + 3] as usize;
-
-                        memory[dest_pos] = if r0 == r1 { 1 } else { 0 };
+                    Op::Eq => {
+                        memory[r2.unwrap() as usize] =
+                            if r0.unwrap() == r1.unwrap() { 1 } else { 0 };
                         pc += 4;
                     }
-                    OP_HALT => return Ok(memory[0]),
-                    x => {
-                        return Err(format!("Unknown opcode: {}", x))
-                    }
+                    Op::Halt => return Ok(()),
                 };
-//                println!("opcode: {}", inst.opcode);
             }
-            Err(e) => panic!(e)
+            Err(e) => return Err(format!("Invalid opcode: {:?}", e)),
         }
     }
-    Ok(memory[0])
+
+    Err(format!("Program halted unexpectedly"))
 }
 
-#[derive(Debug)]
-struct Instruction {
-    opcode: i8,
-    addr_modes: Vec<AddressingMode>
-}
-
-struct Parameter {
-    mode: AddressingMode,
-    value: i32 // interpret based on mode
-}
-
-#[derive(Debug)]
-enum AddressingMode {
-    POSITION,
-    IMMEDIATE,
-}
-
-impl AddressingMode {
-    fn from(i: Option<i8>) -> AddressingMode {
-        match i {
-            None | Some(0) => AddressingMode::POSITION,
-            Some(1) => AddressingMode::IMMEDIATE,
-            Some(x) => panic!("Unknown addressing mode: {}", x)
-        }
-    }
-}
-
-#[derive(Debug)]
-struct InvalidOpcode {
-
-}
-
-impl Error for InvalidOpcode {
-    fn description(&self) -> &str {
-        "Invalid Opcode"
-    }
-}
-
-impl Display for InvalidOpcode {
-    fn fmt(&self, f: &mut Formatter<'_>) -> Result<(), std::fmt::Error> {
-        write!(f, "Invalid opcode")
-    }
-}
-
-/*
-
-    Opcode 5 is jump-if-true: if the first parameter is non-zero, it sets the instruction pointer to the value from the second parameter. Otherwise, it does nothing.
-    Opcode 6 is jump-if-false: if the first parameter is zero, it sets the instruction pointer to the value from the second parameter. Otherwise, it does nothing.
-    Opcode 7 is less than: if the first parameter is less than the second parameter, it stores 1 in the position given by the third parameter. Otherwise, it stores 0.
-    Opcode 8 is equals: if the first parameter is equal to the second parameter, it stores 1 in the position given by the third parameter. Otherwise, it stores 0.
-
-*/
-const OP_ADD: i8 = 1;
-const OP_MUL: i8 = 2;
-const OP_INPUT: i8 = 3;
-const OP_OUTPUT: i8 = 4;
-const OP_JIT: i8 = 5;
-const OP_JIF: i8 = 6;
-const OP_JLT: i8 = 7;
-const OP_EQ: i8 = 8;
-const OP_HALT: i8 = 99;
-
-fn destructure_inst(inst: i32) -> Result<Instruction, InvalidOpcode> {
+fn destructure_inst(inst: i32) -> Result<Instruction, InvalidOpCodeError> {
     let mut digits = digits(inst);
-//    println!("digits: {:?}", digits);
-
     let mut addr_modes: Vec<AddressingMode> = Vec::new();
 
     let opcode_0 = digits.pop_back().unwrap_or_default();
     let opcode_1 = digits.pop_back().unwrap_or_default();
     let opcode: i8 = opcode_1 * 10 + opcode_0;
 
-    for _i in 0..3 {
-        addr_modes.push(AddressingMode::from(digits.pop_back()));
+    let op = Op::from_opcode(opcode)?;
+    for _i in 1..op.num_parameters() {
+        addr_modes.push(AddressingMode::from(digits.pop_back())?);
     }
 
-    Ok(Instruction { opcode, addr_modes })
+    if op.does_store() {
+        addr_modes.push(AddressingMode::DoStore);
+    } else if op.num_parameters() != 0 {
+        addr_modes.push(AddressingMode::from(digits.pop_back())?)
+    }
+
+    Ok(Instruction { op, addr_modes })
+}
+
+fn get_parameter(
+    pc: usize,
+    memory: &Vec<i32>,
+    parm_index: usize, // 0-indexed
+    inst: &Instruction,
+) -> Option<i32> {
+    if parm_index >= inst.op.num_parameters() {
+        return None;
+    } else {
+        let parm_slot = pc + parm_index + 1;
+        let mode = inst.addr_modes.get(parm_index);
+        mode.map(|m| match m {
+            AddressingMode::Immediate => memory[parm_slot],
+            AddressingMode::Position => {
+                let pos = memory[parm_slot];
+                memory[pos as usize]
+            }
+            AddressingMode::DoStore => memory[parm_slot],
+        })
+    }
+}
+
+#[derive(Debug)]
+struct Instruction {
+    op: Op,
+    addr_modes: Vec<AddressingMode>,
+}
+
+#[derive(Debug)]
+enum Op {
+    Add,
+    Mul,
+    Input,
+    Output,
+    Jit,
+    Jif,
+    Lt,
+    Eq,
+    Halt,
+}
+
+/*
+    Opcode 1 adds together numbers read from two positions and stores the result in a third position.
+    Opcode 2 works exactly like opcode 1, except it multiplies the two inputs instead of adding them.
+    Opcode 3 takes a single integer as input and saves it to the position given by its only parameter.
+    Opcode 4 outputs the value of its only parameter. For example, the instruction 4,50 would output the value at address 50.
+    Opcode 5 is jump-if-true: if the first parameter is non-zero, it sets the instruction pointer to the value from the second parameter. Otherwise, it does nothing.
+    Opcode 6 is jump-if-false: if the first parameter is zero, it sets the instruction pointer to the value from the second parameter. Otherwise, it does nothing.
+    Opcode 7 is less than: if the first parameter is less than the second parameter, it stores 1 in the position given by the third parameter. Otherwise, it stores 0.
+    Opcode 8 is equals: if the first parameter is equal to the second parameter, it stores 1 in the position given by the third parameter. Otherwise, it stores 0.
+*/
+impl Op {
+    fn from_opcode(opcode: i8) -> Result<Op, InvalidOpCodeError> {
+        let op = match opcode {
+            1 => Some(Op::Add),
+            2 => Some(Op::Mul),
+            3 => Some(Op::Input),
+            4 => Some(Op::Output),
+            5 => Some(Op::Jit),
+            6 => Some(Op::Jif),
+            7 => Some(Op::Lt),
+            8 => Some(Op::Eq),
+            99 => Some(Op::Halt),
+            _ => None,
+        };
+        op.ok_or_else(|| InvalidOpCodeError {
+            message: format!("Invalid opcode: {}", opcode),
+        })
+    }
+
+    fn num_parameters(&self) -> usize {
+        match self {
+            Op::Add | Op::Mul | Op::Eq | Op::Lt => 3,
+            Op::Jit | Op::Jif => 2,
+            Op::Input | Op::Output => 1,
+            Op::Halt => 0,
+        }
+    }
+
+    fn does_store(&self) -> bool {
+        match self {
+            Op::Add | Op::Mul | Op::Eq | Op::Input | Op::Lt => true,
+            _ => false,
+        }
+    }
+}
+
+#[derive(Debug)]
+enum AddressingMode {
+    Position,
+    Immediate,
+    DoStore,
+}
+
+impl AddressingMode {
+    fn from(i: Option<i8>) -> Result<AddressingMode, InvalidOpCodeError> {
+        match i {
+            Some(0) | None => Ok(AddressingMode::Position),
+            Some(1) => Ok(AddressingMode::Immediate),
+            Some(x) => Err(InvalidOpCodeError {
+                message: format!("Unknown addressing mode: {}", x),
+            }),
+        }
+    }
+}
+
+#[derive(Debug)]
+struct InvalidOpCodeError {
+    message: String,
+}
+
+impl Display for InvalidOpCodeError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result<(), std::fmt::Error> {
+        write!(f, "Invalid opcode {}", self.message)
+    }
 }
 
 fn digits(num: i32) -> VecDeque<i8> {
